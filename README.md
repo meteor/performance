497bf204ee54ab1022db83109ff4edf3373c4e6c
Nacho Codoñer <igcogi@gmail.com>
2024-08-14 18:12:59 +0200

    add command to deploy the apps to galaxy

--- README.md
+++ README.md
@@ -47,3 +47,14 @@ Meteor 3 is in average **~28% faster**, uses **~10% less CPU** and  **~16% less 
 Meteor 3 is in average **~equal on time**, uses **~18% less CPU** and **~10% more of RAM** in a **reactive scenario**.
 
 More details on this benchmark can be found at [`./benchmarks/meteor2-vs-3`](./benchmarks/meteor2-vs-3)
+
+## Deploy
+
+The apps are deployed to galaxy to benefit of further analysis with MontiAPM tool.
+
+To deploy each app:
+
+```shell
+npm install
+./scripts/deploy.sh <app>
+```
---
+++ scripts/deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# app. Application directory name within ./apps/*
+app="${1}"
+if [[ -z "$app" ]]; then
+  echo "Usage: deploy.sh <app_name>"
+  exit 1;
+fi
+
+# Initialize script constants
+baseDir="${PWD}"
+appsDir="${baseDir}/apps"
+appPath="${appsDir}/${app}"
+appPort=3000
+
+# Ensure proper cleanup on interrupt the process
+function cleanup() {
+    builtin cd ${baseDir};
+    exit 0
+}
+trap cleanup SIGINT SIGTERM
+
+# Prepare, run and wait meteor app
+builtin cd "${appPath}"
+rm -rf "${appPath}/.meteor/local"
+hostname="${app//x/0}-perf.meteorapp.com"
+echo "Deploying to ${hostname}"
+if [[ -n "${METEOR_CHECKOUT_PATH}" ]]; then
+  METEOR_PACKAGE_DIRS="${baseDir}/packages" ${METEOR_CHECKOUT_PATH}/meteor deploy ${hostname} --owner mdg
+else
+  METEOR_PACKAGE_DIRS="${baseDir}/packages" meteor deploy ${hostname} --owner mdg
+fi
+cleanup
