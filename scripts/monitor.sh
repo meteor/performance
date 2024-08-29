bc3151de31fccb41349012feced628205faa474c
Nacho Codoñer <igcogi@gmail.com>
2024-08-29 17:55:00 +0200

    improve script to output error on config

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -12,14 +12,20 @@ if [[ -z "$app" ]] || [[ -z "$script" ]]; then
 fi
 
 # Redirect stdout (1) and stderr (2) to a file
+logFile="logs/${logName}-${app}-${script}.log"
 mkdir -p logs
-exec > ./logs/${logName}-${app}-${script}.log 2>&1
+exec > "./${logFile}" 2>&1
 
 # Initialize script constants
 baseDir="${PWD}"
 appsDir="${baseDir}/apps"
 appPath="${appsDir}/${app}"
 appPort=3000
+
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
 # Define helpers
 function getPidByName() {
@@ -44,11 +50,29 @@ function waitMeteorApp() {
 
 # Ensure proper cleanup on interrupt the process
 function cleanup() {
+    verify="${1}"
     builtin cd ${baseDir};
     # Kill all background processes
     pkill -P ${artPid}
     pkill -P $$
-    sleep 5
+    sleep 6
+
+    # Verify valid output
+    if [[ "${verify}" == "true" ]]; then
+      if cat "${baseDir}/${logFile}" | grep -q "TimeoutError"; then
+        echo -e "${RED}*** !!! ERROR: SOMETHING WENT WRONG !!! ***${NC}"
+        echo -e "${RED}Output triggered an unexpected timeout (${logFile})${NC}"
+        echo -e "${RED} Your machine is overloaded and unable to provide accurate comparison results.${NC}"
+        echo -e "${RED} Try switching to a configuration that your machine can handle.${NC}"
+
+        exit 1
+      else
+        echo -e "${GREEN}Output is suitable for comparisons (${logFile})${NC}"
+        echo -e "${GREEN} Your machine managed the configuration correctly.${NC}"
+
+        exit 0
+      fi
+    fi
     exit 0
 }
 trap cleanup SIGINT SIGTERM
@@ -78,4 +102,5 @@ node "${baseDir}/scripts/helpers/monitor-cpu-ram.js" "${dbPid}" "DB" &
 
 # Wait for artillery script to finish the process
 wait "${artPid}"
-cleanup
+
+cleanup "true"
